ef8b0d8aa2510a615129be84a2b4b69fc096ca2f
Sabita <s96558722@gmail.com>
2025-10-16 17:14:14 +0530

    nn

---
+++ array8.js
@@ -0,0 +1,11 @@
+function reverseArray(arr){
+    let n=arr.length;
+    for(let i=0;i<arr.length/2;i++){
+        let temp=arr[i]; //temp=1, temp=2
+    arr[i]=arr[n-i-1];  //arr[i]=7,6
+    arr[n-i-1]=temp;   //arr[n-i-1]=1,2
+}
+}
+const arr=[1,2,3,4,5,6,7]
+reverseArray(arr);
+console.log(arr.join(""))
