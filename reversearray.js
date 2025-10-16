35f37292dba66b77cd5f87bad618c2186b49b110
Sabita <s96558722@gmail.com>
2025-10-16 17:21:27 +0530

    nnn

--- reversearray.js
+++ reversearray.js
@@ -1,17 +1,17 @@
 
-//function reverse(arr){
-   // return arr.reverse();
+function reverse(arr){
+   return arr.reverse();
+}
+let num=[1, 2, 3, 4, 5, 6, 7];
+console.log(reverse(num))
+//function reversearray(arr){
+  //let left=0, right=arr.length-1;
+  //  while(left<right){
+    //    arr[left],arr[right]=arr[right],arr[left]
+    //}
+    //left++;
+    //right--;
 //}
 //let arr=[1, 2, 3, 4, 5, 6, 7];
-//console.log(reverse(arr))
-function reversearray(arr){
-  let left=0, right=arr.length-1;
-    while(left<right){
-        arr[left],arr[right]=arr[right],arr[left]
-    }
-    left++;
-    right--;
-}
-let arr=[1, 2, 3, 4, 5, 6, 7];
-reversearray(arr)
-console.log(arr.join(""))
+//reversearray(arr)
+//console.log(arr.join(""))
