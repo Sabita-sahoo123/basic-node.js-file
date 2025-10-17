3bfc2c951761da0e4303210e1c072a7db0a6eb82
Sabita <s96558722@gmail.com>
2025-10-17 09:02:27 +0530

    a

---
+++ array10.js
@@ -0,0 +1,17 @@
+function findmajority(arr){
+    let n=arr.length-1;
+    for(let i=0;i<n;i++){
+        let count=0;
+        for(j=0;j<n;j++){
+            if(arr[i]===arr[j])
+                count++;
+        }
+        if(count>n/2){
+            return arr[i]
+        }
+    }
+    return -1;
+
+}
+let arr=[1,1,3,2,1,4,2,5]
+console.log(findmajority(arr));
